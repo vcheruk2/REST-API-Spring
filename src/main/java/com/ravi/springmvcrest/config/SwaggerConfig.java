bb005dd3d3c047e8155e2c5e48e9cc902119c96e
Ravi.Cherukuri <vcheruk2@ncsu.edu>
2020-05-17 10:12:12 -0700

    Created API documentation based on Swagger.

---
+++ src/main/java/com/ravi/springmvcrest/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.ravi.springmvcrest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/* Created by: Venkata Ravichandra Cherukuri
+   Created on: 5/17/2020 */
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .pathMapping("/");
+    }
+}
